d722b60c5c7b4ead4f054fccfacbb9044713ca30
Craig McClanahan <craigmcc@gmail.com>
2021-10-13 10:03:09 -0700

    Tweak the home page with initial instructions, based on current login status.

--- client/src/components/views/HomeView.tsx
+++ client/src/components/views/HomeView.tsx
@@ -4,18 +4,35 @@
 
 // External Modules ----------------------------------------------------------
 
-import React from "react";
+import React, {useContext} from "react";
 import Container from "react-bootstrap/Container";
 
 // Internal Modules ----------------------------------------------------------
+
+import LoginContext from "../contexts/LoginContext";
 
 // Component Details ---------------------------------------------------------
 
 const HomeView = () => {
 
+    const loginContext = useContext(LoginContext);
+
     return (
         <Container fluid id="HomeView">
-            <span><strong>Welcome to the CityTeam Guests Checkin App!</strong></span>
+            <p><strong>Welcome to the CityTeam Guests Checkin App!</strong></p>
+            {(loginContext.data.loggedIn) ? (
+                <>
+                <p>Welcome user <strong>{loginContext.data.username}</strong>!</p>
+                <p>Select the <strong>Checkins</strong> link on the Navigation Bar
+                    above to begin checking in Guests.
+                </p>
+                </>
+            ) : (
+               <p>Click the&nbsp;
+                   <img src="/helptext/button-login-top.png" alt="Login Button"/>
+                   &nbsp;and fill in your username and password, to begin.
+               </p>
+            )}
         </Container>
     )
 
